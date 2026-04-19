5477707c7a66f13bf5766a933ccf0e655b815afb
Andrés Julián López Hurtado <andres.lopez.hurtado@gmail.com>
2026-04-19 15:14:19 -0500

    Initial commit

---
+++ src/registro/Registro.jsx
@@ -0,0 +1,7 @@
+import Title from "../components/atoms/Title";
+function Registro({title}) {
+    return (<div className="bg-green-500">
+        <Title title="Registro" />
+    </div>);
+}
+export default Registro;
